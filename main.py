3f7c1bbd8274ba9755a2dd90b27492e58945c195
Sergio Carrera <75439050+Chimeroni@users.noreply.github.com>
2022-12-03 22:28:34 -0800

    Update main.py

--- main.py
+++ main.py
@@ -1,145 +1,108 @@
-from mesa import Agent, Model 
-
-# 'SingleGrid' sirve para forzar a un solo objeto por celda (nuestro objetivo en este "juego")
-from mesa.space import MultiGrid
-
-# 'SimultaneousActivation' habilita la opción de activar todos los agentes de manera simultanea.
-from mesa.time import SimultaneousActivation
-
-# 'DataCollector' permite obtener el grid completo a cada paso (o generación), útil para visualizar
-from mesa.datacollection import DataCollector
-
-# 'matplotlib' lo usamos para graficar/visualizar como evoluciona el autómata celular.
-#%matplotlib inline
-import matplotlib
-import matplotlib.pyplot as plt
-import matplotlib.animation as animation
-#plt.rcParams["animation.html"] = "jshtml"
-#matplotlib.rcParams['animation.embed_limit'] = 2 ** 128
-
-# Definimos los siguientes paquetes para manejar valores númericos: 'numpy' & 'pandas'
-import numpy as np
-import pandas as pd
-
-# Definimos otros paquetes que vamos a usar para medir el tiempo de ejecución de nuestro algoritmo.
-
-import time
-import datetime
-
+import mesa
 import random
+import time
 
-
-
-def get_grid(model):
-    grid = np.zeros((model.grid.width, model.grid.height))
-    for cell in model.grid.coord_iter():
-        cell_content, x, y = cell
-        for obj in cell_content:
-            if isinstance(obj, CleanAgent):
-                grid[x][y] = 2
-            elif isinstance(obj, Cell):
-                grid[x][y] = obj.state
-    return grid
-class CleanAgent(Agent):
-    def __init__(self,unique_id,model):
+class robot_agent(mesa.Agent):
+    def __init__(self, unique_id, model):
         super().__init__(unique_id, model)
-        self.next_state = None
-        self.moves = 0
-        #self.bateria = bateria
-    def advance(self):
-        """
-        Define el nuevo estado calculado del método step.
-        """
-        neighbours = self.model.grid.get_neighbors(
-        self.pos, 
-        moore = True,
-        include_center = True) #Es caso true de Moore, puede moverse libremente
-        for neighbour in neighbours:
-            if isinstance(neighbour, Cell) and self.pos == neighbour.pos:
-                neighbour.state = neighbour.next_state
-                break
-        self.model.grid.move_agent(self, self.next_state)
+
     def step(self):
-        neighbours = self.model.grid.get_neighbors(
-            self.pos,
-            moore=True,
-            include_center=False)
-        for neighbour in neighbours:
-            if isinstance(neighbour, Cell)  and self.pos == neighbour.pos:
-                if neighbour.state:
-                    neighbour.next_state = 0
-                    self.next_state = self.pos
-            else:
-                self.moves += 1
-                neighbours_2 = self.model.grid.get_neighborhood(
-                self.pos, 
-                moore = True,
-                include_center = False)
-                neighbour.next_state = 0
-                ### Implement shortest path ###
-                self.next_state = self.make_decision(neighbours_2)
-            break
-    def make_decision(self,neighbours):
-        """
-        Define la decisión del agente.
-        """
-        return random.choice(neighbours) # Se mueve a celda aledaña random
-class Cell(Agent):
-    def __init__(self,unique_id,model,state):
-        super().__init__(unique_id, model)
-        self.next_state = None
-        self.pos = unique_id
-        self.state = state
-class Board(Model):
-    def __init__(self,m,n,num_agentes,p_sucias):
-        self.num_agentes = num_agentes
-        self.p_sucias = p_sucias
-        self.schedule = SimultaneousActivation(self)
-        self.grid = MultiGrid(m,n,False) # Bool define toroidas
-        num_celdas_sucias = int(m*n*p_sucias) #num de celdas sucias a crear
+        self.move()
+        self.clean()
 
-        #creamos el grid random de 1 y 0
-        for (content,x,y) in self.grid.coord_iter():
-            num = random.randint(0,1)
-            if num == 1 and num_celdas_sucias > 0:
-                a = Cell((x,y), self, 1)
-                num_celdas_sucias -= 1
-            else:
-                a = Cell((x,y), self, 0)
-            self.grid.place_agent(a,(x,y))
+    def clean(self):
+        if not self.model.esta_limpio(self.pos):
+            self.model.cambiar_limpio(self.pos)
+            print("Celda Limpia: ", self.pos, "\n")
+
+    def move(self):
+        possible_steps = self.model.grid.get_neighborhood(self.pos, moore=True, include_center=False)
+        new_position = self.random.choice(possible_steps)
+        self.model.grid.move_agent(self, new_position)
+
+
+class robot_model(mesa.Model):
+    def __init__(self, N, width, height, percent, tiempo_max):
+        self.num_agents = N
+        self.schedule = mesa.time.RandomActivation(self)
+        self.grid = mesa.space.MultiGrid(height, width, True)
+        self.init_time = time.time()
+        self.final_time = tiempo_max
+        self.total_mov = 0
+
+        self.celdas_suc = int((width * height) * percent)
+        self.celdas_lim = int((width * height) * (1 - percent))
+        self.dirty_matrix = [([True]*width) for i in range(height)]
+        self.cant_celdas_suc_inicializar = self.celdas_suc
+        while (self.cant_celdas_suc_inicializar > 0): 
+            for i in range(height):
+                for j in range(width):
+                    sucio_o_limpio = random.randint(0, 1)
+                    if self.cant_celdas_suc_inicializar > 0 and self.dirty_matrix[i][j]:
+                        self.dirty_matrix[i][j] = False
+                        self.cant_celdas_suc_inicializar -= 1
+
+        for i in range(self.num_agents):
+            a = robot_agent(i, self)
             self.schedule.add(a)
-
-        #iniciliza scheduler para los agentes
-        for id in range(num_agentes):
-            r = CleanAgent(id, self)
-            self.grid.place_agent(r, (1,1))
-            self.schedule.add(r)
-
-        self.datacollector = DataCollector(
-            model_reporters = {"Grid":get_grid})
+            self.grid.place_agent(a, (1, 1))
 
     def step(self):
-        self.datacollector.collect(self)
+        self.total_mov += 1
         self.schedule.step()
 
-    def allClean(self):
-        #si todo limpio, terminamos ejecucion
-        for (content, x, y) in self.grid.coord_iter():
-            for obj in content:
-                if isinstance(obj, Cell) and obj.state == 1:
-                    return False
-        return True
+    def total_movimientos(self):
+        return self.total_mov * self.num_agents
 
-    def numDirty(self):
-        cantidad_celdas_sucias = 0
-        for (content, x, y) in self.grid.coord_iter():
-            for obj in content:
-                if isinstance(obj, Cell) and obj.state == 1:
-                    cantidad_celdas_sucias = cantidad_celdas_sucias + 1
-        return cantidad_celdas_sucias
+    def porcentaje_celdas_limpias(self):
+        return self.celdas_lim / (self.celdas_lim + self.celdas_suc)
+    
+    def esta_limpio(self, new_position):
+        x, y = new_position
+        return self.dirty_matrix[x][y]
 
-    def count_moves(self, M, N, num_agentes):
-        cont = 0
-        for i in range (N*M, N*M + num_agentes):
-            cont = cont + self.schedule.agents[i].moves
-        return cont
+    def cambiar_limpio(self, new_position):
+        x, y = new_position
+        self.dirty_matrix[x][y] = True
+        self.celdas_suc -= 1
+        self.celdas_lim += 1
+        if self.celdas_suc == 0:
+            self.final_time = time.time() - self.init_time 
+            self.total_movimientos()
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+
+
+    
+    
+
+# Main - Ejemplo de Entrada
+M = int(input("M: ")) 
+N = int(input("N: ")) 
+
+cant = int(input("# de agentes: "))
+
+suciedad = float(input("% de las celdas sucias: "))
+while (suciedad > 1):
+    suciedad = float(input("Error: Escribe un numero entre 0 y 1: "))
+
+tiempo_max = int(input("\n\n\nIntroduce el tiempo máximo de ejecución (segundos): "))
+
+model = robot_model(cant, N, M, suciedad, tiempo_max)
+while (model.celdas_suc > 0 and ((time.time() - model.init_time) < model.final_time)):
+    model.step()
+    # [print(*line) for line in model.dirty_matrix]
+    
+    for line in model.dirty_matrix:
+        str = ""
+        for l in line:
+            if l == True:
+                str += " 1"
+            else:
+                str += " 0"
+        print(str)
+    print("\nPorcentaje de celdas limpias:", model.porcentaje_celdas_limpias(), "\n\n")
+
+print("Movimientos totales de los agentes:", model.total_movimientos())
+print("Tiempo:", model.final_time, "(s)")
